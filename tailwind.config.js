d5fd8e6333f0f77e06aafc14843515532d41d3f3
Andrew <andrewnovosel90@gmail.com>
2023-08-29 15:48:21 -0400

    Added bg-image and image src in html

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,10 @@ module.exports = {
       colors: {
         red: "#df280f",
       },
+      backgroundImage: {
+        image:
+          "url('https://upload.wikimedia.org/wikipedia/en/thumb/b/ba/Flag_of_Germany.svg/1200px-Flag_of_Germany.svg.png')",
+      },
     },
   },
   plugins: [],
